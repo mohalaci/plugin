b9e83a884b1c1f476a8e3eace8a5bf67347bdb25
takacsl <mohalaci917@gmail.com>
2018-02-19 20:50:57 +0100

    update index.html path

--- pluginserver.js
+++ pluginserver.js
@@ -265,7 +265,7 @@ Method - GET
 Return plugin app
 */
 app.get('/plugin',function(req,res){
-  res.sendFile(path.join(__dirname+'/index.html'));
+  res.sendFile(path.join(__dirname+'/plugin/index.html'));
   //__dirname : It will resolve to your project folder.
 });
 
