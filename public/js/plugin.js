c2b78a30cd32852c43e4ab92eec69014d358136e
barion-vinczei <imre.vincze@barion.com>
2018-02-21 13:43:10 +0100

    status bar scroll hack

--- public/js/plugin.js
+++ public/js/plugin.js
@@ -45,6 +45,9 @@ app.statusbar.setIosTextColor('white');
 app.statusbar.setBackgroundColor('#2196f3');
 
 app.on('init', function(appData) {
+  console.log('init OK');
+  $('body').scrollTop(20);
+  $('.view').scrollTop(20);
   $('.page-content').scrollTop(20); // status bar auto scroll
 });
 
