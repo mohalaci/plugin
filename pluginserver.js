75b79369675f6f3fa551bbe0ecfce6f9306e5d1b
barion-vinczei <imre.vincze@barion.com>
2018-03-09 13:59:32 +0100

    added pagination and dynamic payment request

--- pluginserver.js
+++ pluginserver.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var async = require('async');
-var path    = require("path");
+var path = require("path");
 
 // Create application/x-www-form-urlencoded parser
 var urlencodedParser = bodyParser.urlencoded({ extended: true })
@@ -26,70 +26,70 @@ var BarionRequestBuilderFactory = barion.BarionRequestBuilderFactory;
 /*
 Save plugin into the database
 */
-function saveMarketPlacePlugin(plugin, callback){
-   db.connect(dbUrl, function (err, database) {
-      if (err) throw err
-      var dBase = database.db("plugindb")
-      dBase.collection("marketplaceplugin").insertOne(plugin, function(err, res){
-         if (err) return callback(err)
-         database.close()
-         var result = { "status" : "OK"}
-         callback(null, result)
-      })
-   })
+function saveMarketPlacePlugin(plugin, callback) {
+    db.connect(dbUrl, function (err, database) {
+        if (err) throw err
+        var dBase = database.db("plugindb")
+        dBase.collection("marketplaceplugin").insertOne(plugin, function (err, res) {
+            if (err) return callback(err)
+            database.close()
+            var result = { "status": "OK" }
+            callback(null, result)
+        })
+    })
 }
 
 /*
 Get every plugin from database
 */
-function getPlugins(callback){
-   db.connect(dbUrl, function (err, database) {
-      var plugins = [];
-      if (err) throw err
-      var dBase = database.db("plugindb")
-      var cursor = dBase.collection("marketplaceplugin").find()
-      cursor.each(function(err, item){
-         if (err || item == null) {
-            database.close()
-            if (plugins.length > 0) {
-               callback(null, plugins)
+function getPlugins(callback) {
+    db.connect(dbUrl, function (err, database) {
+        var plugins = [];
+        if (err) throw err
+        var dBase = database.db("plugindb")
+        var cursor = dBase.collection("marketplaceplugin").find()
+        cursor.each(function (err, item) {
+            if (err || item == null) {
+                database.close()
+                if (plugins.length > 0) {
+                    callback(null, plugins)
+                } else {
+                    callback(err)
+                }
             } else {
-               callback(err)
+                var plugin = {
+                    pluginId: item.pluginId,
+                    pluginUrl: item.pluginUrl,
+                    lastUpdatedAt: item.lastUpdatedAt,
+                    resources: {
+                        imageUrl: item.resources.imageUrl,
+                        title: item.resources.title
+                    }
+                }
+                plugins.push(plugin)
+
             }
-         } else  {
-            var plugin = {
-               pluginId : item.pluginId,
-               pluginUrl : item.pluginUrl,
-               lastUpdatedAt : item.lastUpdatedAt,
-               resources : {
-                  imageUrl : item.resources.imageUrl,
-                  title : item.resources.title
-               }
-            }
-            plugins.push(plugin)
-            
-         }
-      })
-   })
+        })
+    })
 }
 
 /**
 Delete every plugin from database
 */
-function clearPluginCollection(callback){
-   db.connect(dbUrl, function (err, database) {
-      var users = [];
-      if (err) throw err
-      var dBase = database.db("plugindb")
-      var cursor = dBase.collection("marketplaceplugin").deleteMany({}, function(err, res){
-         if (err) {
-            callback(err)
-         } else {
-            callback(null, res)
-         }
-         database.close()
-      })
-   })
+function clearPluginCollection(callback) {
+    db.connect(dbUrl, function (err, database) {
+        var users = [];
+        if (err) throw err
+        var dBase = database.db("plugindb")
+        var cursor = dBase.collection("marketplaceplugin").deleteMany({}, function (err, res) {
+            if (err) {
+                callback(err)
+            } else {
+                callback(null, res)
+            }
+            database.close()
+        })
+    })
 }
 
 app.use(express.static('public'));
@@ -98,60 +98,60 @@ app.use(express.static('public'));
 Method - GET
 Generate a payment in Barion Test environment and return the paymentId parameter
 */
-app.get('/genpayment', function(req, res){
-   var paymentStartRequestBuilder  = new BarionRequestBuilderFactory.BarionPaymentStartRequestBuilder();
+app.get('/genpayment', function (req, res) {
+    var paymentStartRequestBuilder = new BarionRequestBuilderFactory.BarionPaymentStartRequestBuilder();
+    
+    var paymentStartOptionsWithObject = {
+        POSKey: "ec5abfa2-5eea-42db-9568-b9a4cf825b88",
+        PaymentType: "Immediate",
+        GuestCheckOut: true,
+        FundingSources: ["All"],
+        PaymentRequestId: "request_id_generated_by_the_shop",
+        Locale: "hu-HU",
+        Currency: "HUF",
+        Transactions: [
+            {
+                POSTransactionId: "test_payment_id_from_shop",
+                Payee: "testshop@barion.com",
+                Total: req.bookPrice,
+                Items: [
+                    {
+                        Name: req.bookAuthor+": "+req.bookTitle,
+                        Description: req.bookPublisher+", ISBN "+req.bookISBN,
+                        Quantity: 1,
+                        Unit: "db",
+                        UnitPrice: req.bookPrice,
+                        ItemTotal: req.bookPrice
+                    }
+                ]
+            }
+        ]
+    };
 
-var paymentStartOptionsWithObject = {
-    POSKey: "ec5abfa2-5eea-42db-9568-b9a4cf825b88",
-    PaymentType: "Immediate",
-    GuestCheckOut: true,
-    FundingSources: ["All"],
-    PaymentRequestId: "request_id_generated_by_the_shop",
-    Locale: "hu-HU",
-    Currency: "HUF",
-    Transactions: [
-        {
-            POSTransactionId: "test_payment_id_from_shop",
-            Payee: "testshop@barion.com",
-            Total: "1000",
-            Items: [
-                {
-                    Name: "Test product",
-                    Description: "My favorite test product",
-                    Quantity: 1,
-                    Unit: "db",
-                    UnitPrice: 1000,
-                    ItemTotal: 1000
+    var paymentData;
+    async.series([
+        function (callback) {
+            barion.startPayment(paymentStartOptionsWithObject, function (err, data) {
+                if (err) {
+                    paymentData = "errror";
+                    callback()
+                } else {
+                    console.log("paymentstart result, data: ")
+                    console.log(data)
+                    callback(data)
                 }
-            ]
-        }
-    ]
-};
-
-var paymentData;
-async.series([
-   function(callback){
-      barion.startPayment(paymentStartOptionsWithObject, function (err, data) {
-    if (err) {
-      paymentData = "errror";
-      callback()
-    } else {
-      console.log("paymentstart result, data: ")
-      console.log(data)
-      callback(data)
-    }
-})
-   }], function(data){
-      res.setHeader('Content-Type','application/json')
-    if (data) {
-            res.status(200)
-            console.log(JSON.stringify({paymentId:data.PaymentId}))
-         res.json({paymentId:data.PaymentId})
-    } else {
-        res.status(500)
-        res.json({ERROR:"ERROR"})
-    }
-   })
+            })
+        }], function (data) {
+            res.setHeader('Content-Type', 'application/json')
+            if (data) {
+                res.status(200)
+                console.log(JSON.stringify({ paymentId: data.PaymentId }))
+                res.json({ paymentId: data.PaymentId })
+            } else {
+                res.status(500)
+                res.json({ ERROR: "ERROR" })
+            }
+        })
 
 });
 
@@ -159,111 +159,120 @@ async.series([
 Method - POST
 Add new plugin to the database
 */
-app.post('/addplugin', urlencodedParser, function(req, res) {
-   var dbResult;
-   async.series([
-      function(callback){
-         var plugin = {
-            pluginId : req.body.pluginId,
-            lastUpdatedAt : req.body.lastUpdatedAt,
-            pluginUrl : req.body.pluginUrl,
-            resources : {
-               imageUrl : req.body.resources.imageUrl,
-               title : req.body.resources.title
+app.post('/addplugin', urlencodedParser, function (req, res) {
+    var dbResult;
+    async.series([
+        function (callback) {
+            var plugin = {
+                pluginId: req.body.pluginId,
+                lastUpdatedAt: req.body.lastUpdatedAt,
+                pluginUrl: req.body.pluginUrl,
+                resources: {
+                    imageUrl: req.body.resources.imageUrl,
+                    title: req.body.resources.title
+                }
             }
-         }
-         saveMarketPlacePlugin(plugin, function(err, result){
-            if (err) {
-               dbResult = {status : "ERROR"}
-               return callback(dbResult)
-            } else {
-               dbResult = {status : "OK"}
-               callback()
-            }
-         })
-      }], function(err){
-         res.setHeader('Content-Type','application/json')
+            saveMarketPlacePlugin(plugin, function (err, result) {
+                if (err) {
+                    dbResult = { status: "ERROR" }
+                    return callback(dbResult)
+                } else {
+                    dbResult = { status: "OK" }
+                    callback()
+                }
+            })
+        }], function (err) {
+            res.setHeader('Content-Type', 'application/json')
 
-         if (err) {
-            res.status(500)
-            res.json(err)
-         } else {
-            res.status(200)
-            res.json(dbResult)   
-         }
-      })
+            if (err) {
+                res.status(500)
+                res.json(err)
+            } else {
+                res.status(200)
+                res.json(dbResult)
+            }
+        })
 })
 
 /*
 Method - GET
 Return every plugin from database
 */
-app.get('/getplugins', function(req, res){
-   var dbResult;
-   async.series([
-      function(callback){
-         getPlugins(function(err, result){
-         if (err) {
-               dbResult = {status : "FAILED"}
-               return callback(dbResult)
+app.get('/getplugins', function (req, res) {
+    var dbResult;
+    async.series([
+        function (callback) {
+            getPlugins(function (err, result) {
+                if (err) {
+                    dbResult = { status: "FAILED" }
+                    return callback(dbResult)
+                }
+                dbResult = result
+                callback()
+            })
+        }], function (err) {
+            res.setHeader('Content-Type', 'application/json')
+            if (err) {
+                res.status(500)
+            } else {
+                res.status(200)
             }
-            dbResult = result
-            callback()
-         })
-      }], function(err){
-         res.setHeader('Content-Type','application/json')
-         if (err) {
-            res.status(500)
-         } else {
-            res.status(200)
-         }
-            console.log(JSON.stringify({plugins:dbResult}))
-         res.json({plugins:dbResult})
-      })
+            console.log(JSON.stringify({ plugins: dbResult }))
+            res.json({ plugins: dbResult })
+        })
 })
 
 /*
 Method - POST
 Delete every plugin from database
 */
-app.post('/clearplugins', function(req, res){
-   var dbResult;
-   async.series([
-      function(callback){
-         clearPluginCollection(function(err, result){
-            if (err) {
-               return callback(err)
-            } else {
-               dbResult = result
-               return callback(null, result)
-            }
-         })
-      }
-      ], function(err){
-         res.setHeader('Content-Type','application/json')
-         if (err) {
-               res.status(500)
-            } else {
-               res.status(200)
-            }
-         res.json(dbResult)
-      })
+app.post('/clearplugins', function (req, res) {
+    var dbResult;
+    async.series([
+        function (callback) {
+            clearPluginCollection(function (err, result) {
+                if (err) {
+                    return callback(err)
+                } else {
+                    dbResult = result
+                    return callback(null, result)
+                }
+            })
+        }
+    ], function (err) {
+        res.setHeader('Content-Type', 'application/json')
+        if (err) {
+            res.status(500)
+        } else {
+            res.status(200)
+        }
+        res.json(dbResult)
+    })
 })
 
 /*
 Method - GET
 Return plugin app
 */
-app.get('/plugin',function(req,res){
-  res.sendFile(path.join(__dirname+'/public/index.html'));
-  //__dirname : It will resolve to your project folder.
+app.get('/plugin', function (req, res) {
+    res.sendFile(path.join(__dirname + '/public/index.html'));
+    //__dirname : It will resolve to your project folder.
 });
 
+app.get('/booklist', function (req, res) {
+    res.sendFile(path.join(__dirname + '/public/index.html'));
+    //__dirname : It will resolve to your project folder.
+});
+
+app.get('/bookdetails', function (req, res) {
+    res.sendFile(path.join(__dirname + '/public/bookdetails.html'));
+    //__dirname : It will resolve to your project folder.
+});
 
 var server = app.listen(8083, function () {
-   var host = server.address().address
-   var port = server.address().port
+    var host = server.address().address
+    var port = server.address().port
 
-   console.log("Plugin server listening at http://%s:%s", host, port)
+    console.log("Plugin server listening at http://%s:%s", host, port)
 
 })
--- public/js/plugin.js
+++ public/js/plugin.js
@@ -1,3 +1,5 @@
+var $selectedBook = 0;
+
 var app = new Framework7({
     root: '#app',
     name: 'My App',
@@ -6,6 +8,23 @@ var app = new Framework7({
         swipe: 'left',
     },
     routes: [
+        {
+            name: 'bookdetails',
+            path: '/bookdetails/',
+            url: 'bookdetails.html',
+            on: {
+                pageInit: function (e, page) {
+                    if ($selectedBook > 0) {
+                        var bookData = books[$selectedBook - 1];
+                        var bookDetailsTemplate = $$('script#detailsTemplate').html();
+                        var compiledTemplate = Template7.compile(bookDetailsTemplate);
+                        var content = compiledTemplate(bookData);
+                        $(".book-template").html(content);
+                    }
+                }
+            },
+            pushState: true
+        },
         {
             path: '/done.html',
             url: 'done.html'
@@ -33,7 +52,7 @@ var mainView = app.views.create('.view-main');
 app.statusbar.setIosTextColor('white');
 app.statusbar.setBackgroundColor('#1A80BB');
 
-app.on('init', function(){
+app.on('init', function() {
     if ($('html.ios').length > 0) {
         $('body').scrollTop(20);
         $('.view').scrollTop(20);
@@ -43,9 +62,20 @@ app.on('init', function(){
 });
 
 $(document).ready(function () {
+    var bookListTemplate = $$('script#listTemplate').html();
+    var compiledTemplate = Template7.compile(bookListTemplate);
+    var content = compiledTemplate({ books: books });
+    $(".list-template").html(content);
+    
     $(document).on('click', "#payWithBarionButton", generatePaymentId);
     $(document).on('click', "#resultButton", closePlugin);
-    $(document).on('click', "#backButton", closePlugin);
+    $(document).on('click', "#exitButton", closePlugin);
+    $(document).on('click', ".back-link", function () {
+        mainView.router.back();
+    });
+    $(document).on('click', ".book-link", function () {
+        $selectedBook = $(this).attr("data-book-id");
+    });
     
     $(document).on('click', ".navbar, .statusbar", function(e) {
         e.preventDefault();
@@ -73,9 +103,17 @@ function postToBarionHandler(obj) {
 
 function generatePaymentId() {
     $("#payWithBarionButton").addClass('disabled').attr('disabled', 'disabled');
+    var book = books[$selectedBook - 1];
     $.ajax({
         method: "GET",
-        url: "https://plugin.mobileappdev.org/genpayment",
+        url: "/genpayment",
+        data: {
+            bookAuthor: book.bookAuthor,
+            bookTitle: book.bookTitle,
+            bookISBN: book.bookISBN,
+            bookPublisher: book.bookPublisher,
+            bookPrice: book.bookPrice
+        },
         dataType: "json",
         error: function(xhr, status, error) {
             alert("ERROR: " + error + "\r\nStatus: " + status);
