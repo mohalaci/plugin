443b0adcdccfbbed44bb8457b82c07731a19fcb9
barion-vinczei <imre.vincze@barion.com>
2018-03-10 11:07:30 +0100

    added shipping address management and summary page before payment

--- public/js/plugin.js
+++ public/js/plugin.js
@@ -1,4 +1,22 @@
 var $selectedBook = 0;
+var $shippingAddress = null;
+var $defaultShipping = {
+    "stairway": "A",
+    "lastName": "Doe",
+    "prefix": "Dr.",
+    "firstName": "John",
+    "street": "Teszt utca",
+    "type": "Private",
+    "comment": "",
+    "city": "Budapest",
+    "postalCode": "1234",
+    "streetNumber": "32.",
+    "floor": "2",
+    "taxNumber": "10203040",
+    "country": "Hungary",
+    "door": "5",
+    "companyName": "Test Inc."
+};
 
 var app = new Framework7({
     root: '#app',
@@ -8,6 +26,21 @@ var app = new Framework7({
         swipe: 'left',
     },
     routes: [
+        {
+            name: 'booklist',
+            path: '/booklist/',
+            page: 'booklist',
+            url: '/',
+            on: {
+                pageInit: function (e, page) {
+                    var bookListTemplate = $$('script#listTemplate').html();
+                    var compiledTemplate = Template7.compile(bookListTemplate);
+                    var content = compiledTemplate({ books: books });
+                    $(".list-template").html(content);
+                }
+            },
+            pushState: true
+        },
         {
             name: 'bookdetails',
             path: '/bookdetails/',
@@ -20,6 +53,27 @@ var app = new Framework7({
                         var compiledTemplate = Template7.compile(bookDetailsTemplate);
                         var content = compiledTemplate(bookData);
                         $(".book-template").html(content);
+                    }
+                }
+            },
+            pushState: true
+        },
+        {
+            name: 'summary',
+            path: '/summary/',
+            url: '/summary.html',
+            on: {
+                pageInit: function (e, page) {
+                    if ($selectedBook > 0 && $shippingAddress != null) {
+                        var bookData = books[$selectedBook - 1];
+                        var summaryData = {
+                            book: bookData,
+                            shipping: $shippingAddress
+                        };
+                        var summaryTemplate = $$('script#summaryTemplate').html();
+                        var compiledTemplate = Template7.compile(summaryTemplate);
+                        var content = compiledTemplate(summaryData);
+                        $(".summary-template").html(content);
                     }
                 }
             },
@@ -52,7 +106,7 @@ var mainView = app.views.create('.view-main');
 app.statusbar.setIosTextColor('white');
 app.statusbar.setBackgroundColor('#1A80BB');
 
-app.on('init', function() {
+app.on('init', function () {
     if ($('html.ios').length > 0) {
         $('body').scrollTop(20);
         $('.view').scrollTop(20);
@@ -66,24 +120,25 @@ $(document).ready(function () {
     var compiledTemplate = Template7.compile(bookListTemplate);
     var content = compiledTemplate({ books: books });
     $(".list-template").html(content);
-    
-    $(document).on('click', "#payWithBarionButton", generatePaymentId);
+
+    $(document).on('click', "#payWithBarionButton", getPaymentId);
+    $(document).on('click', "#setShippingButton", getShippingAddress);
     $(document).on('click', "#resultButton", closePlugin);
     $(document).on('click', "#exitButton", closePlugin);
 
     $(document).on('click', ".book-list-item", function () {
         $selectedBook = $(this).attr("data-book-id");
-        mainView.router.navigate('/bookdetails/');
+        mainView.router.navigate("/bookdetails/");
     });
 
     $(document).on('click', ".back-link", function () {
-        mainView.router.back();
+        mainView.router.navigate("/booklist/");
     });
     $(document).on('click', ".book-link", function () {
         $selectedBook = $(this).attr("data-book-id");
     });
-    
-    $(document).on('click', ".navbar, .statusbar", function(e) {
+
+    $(document).on('click', ".navbar, .statusbar", function (e) {
         e.preventDefault();
         e.stopPropagation();
         return false;
@@ -96,9 +151,9 @@ function postToBarionHandler(obj) {
     if (typeof barionPluginHandler != "undefined") {
         handler = barionPluginHandler;
     } else {
-        handler = (typeof window.webkit != "undefined" 
-                && typeof window.webkit.messageHandlers != "undefined" 
-                && typeof window.webkit.messageHandlers.barionPluginHandler != "undefined") ? window.webkit.messageHandlers.barionPluginHandler : null;
+        handler = (typeof window.webkit != "undefined"
+            && typeof window.webkit.messageHandlers != "undefined"
+            && typeof window.webkit.messageHandlers.barionPluginHandler != "undefined") ? window.webkit.messageHandlers.barionPluginHandler : null;
     }
     if (typeof handler != "undefined" && handler != null) {
         handler.postMessage(message);
@@ -107,7 +162,7 @@ function postToBarionHandler(obj) {
     }
 }
 
-function generatePaymentId() {
+function getPaymentId() {
     $("#payWithBarionButton").addClass('disabled').attr('disabled', 'disabled');
     var book = books[$selectedBook - 1];
     $.ajax({
@@ -121,13 +176,13 @@ function generatePaymentId() {
             bookPrice: book.bookPrice
         },
         dataType: "json",
-        error: function(xhr, status, error) {
+        error: function (xhr, status, error) {
             alert("ERROR: " + error + "\r\nStatus: " + status);
         },
-        success: function(data, status, xhr) {
+        success: function (data, status, xhr) {
             if (status == "success") {
                 var messageObj = {
-                    'action': 'Pay', 
+                    'action': 'Pay',
                     'paymentId': data.paymentId
                 };
                 postToBarionHandler(messageObj);
@@ -135,10 +190,34 @@ function generatePaymentId() {
                 alert("Request finished with status code '" + status + "', could not process response.");
             }
         },
-        complete: function() {
+        complete: function () {
             $("#payWithBarionButton").removeClass('disabled').removeAttr('disabled');
         }
     });
+}
+
+function getShippingAddress() {
+    var shippingObj = { 'action': 'setshipping' };
+    postToBarionHandler(shippingObj);
+}
+
+function setShippingAddress(shippingData) {
+    if (typeof shippingData != "undefined" && shippingData != null) {
+        var s = JSON.parse(shippingData);
+        $shippingAddress = {
+            country: s.country,
+            city: s.city,
+            street: s.street,
+            streetNumber: s.streetNumber,
+            zip: s.postalCode,
+            firstName: s.firstName,
+            lastName: s.lastName,
+            companyName: s.companyName
+        };
+    } else {
+        $shippingAddress = $defaultShipping;
+    }
+    mainView.router.navigate("/summary/");
 }
 
 function successfulPaymentCallback(data) {
@@ -150,6 +229,6 @@ function unSuccessfulPaymentCallback(data) {
 }
 
 function closePlugin() {
-    var closeObj = {'action':'close'};
+    var closeObj = { 'action': 'close' };
     postToBarionHandler(closeObj);
 }
